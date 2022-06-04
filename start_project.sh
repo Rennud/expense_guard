3bfa7eeda38fa72f1ace8fa832b46236e33ffa90
duner <rennud>
2022-06-04 19:03:47 +0200

    script for starting local dev env

---
+++ start_project.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "1 for FE, 2 for BE, 3 for quit: "
+read CONFIRMATION
+
+# rum FE
+if [ $CONFIRMATION == '1' ]; then 
+    cd client
+    yarn start 
+
+# run BE
+elif [ $CONFIRMATION == '2' ]; then 
+    # activate venv
+    cd server/venv/bin
+    source activate
+
+    # run flask app 
+    cd ../../
+    flask run --no-debugger
+
+# exit     
+elif [ $CONFIRMATION == '3' ]; then 
+    echo "Code more next time."
+    return
+fi
+
+deactivate
+cd ~ && cd expense_guard
